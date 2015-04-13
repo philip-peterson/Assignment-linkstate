eb9a68ec05ce7c0f65b9b8db3e533efa3b1cd382
Philip Peterson <ironmagma@Philips-MacBook-Pro.local>
2015-04-13 16:50:39 -0400

    More work, but stuck on weird bug...

--- linkstate.c
+++ linkstate.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <limits.h>
 
 #define ERR_USAGE 1
 #define ERR_IO    2
+#define ERR_OOM   3
+#define ERR_INCON 4
+#define ERR_ZEROE 5
+#define ERR_INVAL 6
 
 #define TOK_NEWLINE  -1
 #define TOK_COMMA    -2
@@ -57,6 +62,69 @@ int initializeBuffers(const char *fp) {
       struct node *next;
    };
 
+   struct node *tail = NULL;
+
+   int N = 0;
+   int count = 0;
+   int row = 0;
+
+   while (1) {
+      int token = getTok(stream);
+      if (token >= 0 || token == TOK_N) {
+         struct node *newtail = malloc(sizeof(*tail));
+         if (newtail == NULL) {
+            fprintf(stderr, "Error: out of memory.\n");
+            exit(ERR_OOM);
+         }
+         count++;
+         newtail->next = NULL;
+         if (token == TOK_N) {
+            newtail->val = INT_MAX;
+         }
+         else {
+            newtail->val = token;
+         }
+
+         if (tail == NULL) {
+            tail = newtail;
+         }
+         else {
+            tail->next = newtail;
+         }
+      }
+      else if (token == TOK_COMMA) {
+         continue;
+      }
+      else if (token == TOK_NEWLINE) {
+         // printf("Newline!\n"); // uncommenting this line makes the program terminate...
+
+         if (row == 0) {
+            N = count;
+            if (N == 0) {
+               fprintf(stderr, "Error: zero elements in first row.\n");
+               exit(ERR_ZEROE);
+            }
+         }
+         else {
+            if (count != N) {
+               fprintf(stderr, "Error: inconsistent element count.\n");
+               exit(ERR_INCON);
+            }
+         }
+
+         row++;
+         count = 0;
+         continue;
+      }
+      else if (token == TOK_EOF) {
+         break;
+      }
+      else {
+         fprintf(stderr, "Error: invalid input.\n");
+         exit(ERR_INVAL);
+      }
+   }
+
    if (fclose(stream) != 0) {
       return 0;
    }
@@ -77,4 +145,6 @@ int main(int argc, char **argv) {
       free(err);
       return ERR_IO;
    }
+
+   return 0;
 }
